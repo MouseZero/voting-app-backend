fb9404c49bdab3ea29cd066f2d6fd02de80a54af
mousezero <rm@mousezero.com>
2016-12-09 21:16:49 -0800

    added parsing for json and erlencoding

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const config = require('./config');
 
 app.set('superSecret', config.secret);
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Welcome to a great api');
 })
 
 app.listen(3333, function(){
