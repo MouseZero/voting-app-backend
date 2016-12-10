e46eb144d415bb91fad9505406f13d218bf2629b
mousezero <rm@mousezero.com>
2016-12-09 21:11:28 -0800

    Now looks for config file with secret

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const config = require('./config');
+
+app.set('superSecret', config.secret);
 
 app.get('/', function(req, res){
   res.send('Hello World');
 })
 
 app.listen(3333, function(){
-  console.log('started server')
+  console.log('started server');
 })
