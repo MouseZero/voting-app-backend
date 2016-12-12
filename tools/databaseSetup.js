b4c88ca105d366ed1f02edc234c93c7bc7c9bdcc
mousezero <rm@mousezero.com>
2016-12-11 23:10:50 -0800

    databaseSetup tool is now setup to run on its own

--- tools/databaseSetup.js
+++ tools/databaseSetup.js
@@ -1,30 +1,27 @@
 const pg = require('pg');
 const config = require('../config');
 
+const dbPoolSettings = {
+  user: config.database.user,
+  database: config.database.database,
+  password: config.database.password,
+  host: config.database.host,
+  port: config.database.port,
+  max: config.database.connectionNumber,
+  idleTimeoutMillis: config.database.idleTimeoutMillis,
+};
 
-module.exports = function databaseSetup(){
-  const dbPoolSettings = {
-    user: config.database.user,
-    database: config.database.database,
-    password: config.database.password,
-    host: config.database.host,
-    port: config.database.port,
-    max: config.database.connectionNumber,
-    idleTimeoutMillis: config.database.idleTimeoutMillis,
-  };
+const pool = new pg.Pool(dbPoolSettings);
 
-  const pool = new pg.Pool(dbPoolSettings);
+pool.connect( function(err, client, done){
+  if(err){
+    return console.error('Error fetching client pool for postgres db', err);
+  }
 
-  pool.connect( function(err, client, done){
+  client.query('CREATE TABLE "user" ( id serial NOT NULL, "user" character(30) NOT NULL, password character(100) NOT NULL, CONSTRAINT unique_id UNIQUE (id), CONSTRAINT unique_user UNIQUE ("user") ) WITH ( OIDS=FALSE );', [], function(err, result){
+    done();
     if(err){
-      return console.error('Error fetching client pool for postgres db', err);
+      return console.error('error running query', err);
     }
-
-    client.query('CREATE TABLE "user" ( id serial NOT NULL, "user" character(30) NOT NULL, password character(100) NOT NULL, CONSTRAINT unique_id UNIQUE (id), CONSTRAINT unique_user UNIQUE ("user") ) WITH ( OIDS=FALSE );', [], function(err, result){
-      done();
-      if(err){
-        return console.error('error running query', err);
-      }
-    })
-  });
-}
+  })
+});
