262de9d2dee831501271f0e1e4e8535499a4bc88
mousezero <rm@mousezero.com>
2016-12-12 20:26:24 -0800

    Added COR headers

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.set('superSecret', config.secret);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//CORS
+app.use(function (req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
+
 app.get('/', function(req, res){
   res.send('Welcome to a great api');
 })
