f5464993e1423cd8df24b2b76ac6e9cf360479b9
mousezero <rm@mousezero.com>
2016-12-13 20:35:47 -0800

    Adding a chart table to the database

--- tools/databaseSetup.js
+++ tools/databaseSetup.js
@@ -7,10 +7,25 @@ pool.connect( function(err, client, done){
     return console.error('Error fetching client pool for postgres db', err);
   }
 
-  client.query('CREATE TABLE "user" ( id serial NOT NULL, "user" character(30) NOT NULL, password character(100) NOT NULL, CONSTRAINT unique_id UNIQUE (id), CONSTRAINT unique_user UNIQUE ("user") ) WITH ( OIDS=FALSE );', [], function(err, result){
-    done();
-    if(err){
-      return console.error('error running query', err);
-    }
-  })
+  client.query(
+    //Create User Table
+    'CREATE TABLE "user" ' + 
+      '( id serial NOT NULL, ' +
+      '"user" character(30) NOT NULL, ' +
+      'password character(100) NOT NULL, ' +
+      'CONSTRAINT unique_id UNIQUE (id), ' +
+      'CONSTRAINT unique_user UNIQUE ("user") ) WITH ( OIDS=FALSE ); ' +
+    //Create Chart Table
+      'CREATE TABLE charts(' +
+      'id serial NOT NULL,' +
+      'title character(150),' +
+      'description character(400),' +
+      'data json)WITH (  OIDS=FALSE);', 
+    [], 
+    function(err, result){
+      done();
+      if(err){
+        return console.error('error running query', err);
+      }
+  });
 });
