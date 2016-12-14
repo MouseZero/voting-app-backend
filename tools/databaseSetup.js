b332416f35f331f66e9f3237569c3f0232f53ee6
mousezero <rm@mousezero.com>
2016-12-13 21:59:15 -0800

    Cleaned up database query format

--- tools/databaseSetup.js
+++ tools/databaseSetup.js
@@ -9,18 +9,37 @@ pool.connect( function(err, client, done){
 
   client.query(
     //Create User Table
-    'CREATE TABLE "user" ' + 
-      '( id serial NOT NULL, ' +
-      '"user" character(30) NOT NULL, ' +
-      'password character(100) NOT NULL, ' +
-      'CONSTRAINT unique_id UNIQUE (id), ' +
-      'CONSTRAINT unique_user UNIQUE ("user") ) WITH ( OIDS=FALSE ); ' +
+    `
+    CREATE TABLE "user"
+    (
+      id serial NOT NULL,
+      "user" character(30) NOT NULL,
+      password character(100) NOT NULL,
+      CONSTRAINT unique_id PRIMARY KEY (id),
+      CONSTRAINT unique_user UNIQUE ("user")
+    )
+    WITH (
+      OIDS=FALSE
+    );
+    ` +
     //Create Chart Table
-      'CREATE TABLE charts(' +
-      'id serial NOT NULL,' +
-      'title character(150),' +
-      'description character(400),' +
-      'data json)WITH (  OIDS=FALSE);', 
+    `
+    CREATE TABLE charts
+    (
+      id serial NOT NULL,
+      title character(150),
+      description character(400),
+      data json,
+      "userId" integer NOT NULL,
+      CONSTRAINT charts_pkey PRIMARY KEY (id),
+      CONSTRAINT lnk_user_charts FOREIGN KEY ("userId")
+          REFERENCES "user" (id) MATCH FULL
+          ON UPDATE RESTRICT ON DELETE CASCADE
+    )
+    WITH (
+      OIDS=FALSE
+    );
+    `, 
     [], 
     function(err, result){
       done();
